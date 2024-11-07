d647b99c0ac3aa192370d4c108a6946858a5fc67
BaiTozi <miti0504@gmail.com>
2024-11-07 21:26:25 +0300

    add header

---
+++ MegaNutrition/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+          <h2>Order your food supplemts here</h2>
+          <p>Ne znam</p>
+          <button>View supplemts</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
