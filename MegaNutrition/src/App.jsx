3d99cf3e9c108b8fe20dc7b26d08e93942aa894a
BaiTozi <miti0504@gmail.com>
2024-11-11 21:43:22 +0300

    added route

--- MegaNutrition/src/App.jsx
+++ MegaNutrition/src/App.jsx
@@ -4,17 +4,26 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import ReactDOM from 'react-dom/client'
+import LoginPopup from './components/LoginPage/LoginPopup'
+import { useState } from 'react';
 
 const App = () => {
+
+  const [showLogin,setShowLogin] = useState(false)
+
   return (
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
     <div className='app'>
-        <Navbar/>
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path='/' element={<Home/>}/>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/order' element={<PlaceOrder/>}/>
-        </Routes>
+        </Routes>        
     </div>
+    </>
   )
 }
 
